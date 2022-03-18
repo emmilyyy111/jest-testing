359753320ced5f3c67f63fd15e6543058da598b2
emilymorgenbesser <emorgenbesser@sigmasolve.net>
2022-03-18 09:47:52 -0400

    testing-1

---
+++ js/cloneArray.js
@@ -0,0 +1,6 @@
+const cloneArray = (array) => {
+    return [...array]
+}
+console.log(cloneArray([1,4,8,3,5]))
+
+module.exports = cloneArray
---
+++ js/subtractNumbers.js
@@ -0,0 +1,6 @@
+function subtractNums (a,b) {
+    return a-b
+}
+console.log(subtractNums(18,3))
+
+module.exports = subtractNums
